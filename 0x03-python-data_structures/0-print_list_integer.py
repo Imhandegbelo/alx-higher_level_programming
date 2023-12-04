be2f791379df12056bf8f45c418654849fe9f055
Imhandegbelo <okogbe@github.com>
2023-12-04 02:25:42 -0800

    fix: pycodestyle fixes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,9 @@
+'''
+Function to prints all integers in a list
+@my_list=[] : list from which to print number
+'''
+
+
 def print_list_integer(my_list=[]):
     for num in my_list:
         print("{:d}".format(num))
