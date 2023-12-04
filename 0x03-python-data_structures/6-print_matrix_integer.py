40742efa798eef64e5fc1dd32d65aa7dd2ec5fd6
Imhandegbelo <okogbe@github.com>
2023-12-04 04:38:50 -0800

    update: added 6-print-matrix-integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+function that prints a matrix of integers
+'''
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if col != len(matrix[0]) - 1:
+                print('{:d} '.format(matrix[row][col]), end=' ')
+            else:
+                print('{:d}'.format(matrix[row][col]), end='')
+        print()
