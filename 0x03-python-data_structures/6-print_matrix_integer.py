de8a0ad1bdcd6fde901ba85c9b0140a439b899a8
Imhandegbelo <okogbe@github.com>
2023-12-04 04:44:29 -0800

    fix: checker error - edge case for empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,10 +5,11 @@ function that prints a matrix of integers
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            if col != len(matrix[0]) - 1:
-                print('{:d} '.format(matrix[row][col]), end=' ')
-            else:
-                print('{:d}'.format(matrix[row][col]), end='')
-        print()
+    if matrix:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if col != len(matrix[0]) - 1:
+                    print('{:d} '.format(matrix[row][col]), end=' ')
+                else:
+                    print('{:d}'.format(matrix[row][col]), end='')
+            print()
