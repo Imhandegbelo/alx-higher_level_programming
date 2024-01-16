ccd8870d823e40c882089e8460b431918b0ad7a2
Imhandegbelo <okogbe@github.com>
2024-01-16 04:36:28 -0800

    update: added caesar

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,6 +7,7 @@ def add_integer(a, b=98):
        Args: a and b
          If not integers or floats raise TypeError
     """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
