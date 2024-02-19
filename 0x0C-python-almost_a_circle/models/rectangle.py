b3d66e6069d3991748d187eae469cbac4a634531
Imhandegbelo <okogbe@github.com>
2024-02-19 07:13:46 -0800

    update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,54 @@ class Rectangle(Base):
         return "[Rectangle] " + "(" + str(self.id) + ") " + str(self.__x)\
                + "/" + str(self.__y) + " - " + str(self.__width)\
                + "/" + str(self.__height)
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+
+           Args:
+                *args (int): The values replacing the attributes
+                **kwargs (dict): Key, Value pairs of the attributes
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
